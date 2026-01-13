17db133117dd2edc9ff0f204ed2677ecdaed17a7
galaxlabs <galaxylab2020@gmail.com>
2026-01-13 17:18:12 +0000

    update

---
+++ rshpm_system/rshpm_system/doctype/ownership_transfer/ownership_transfer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Galaxy Labs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ownership Transfer", {
+// 	refresh(frm) {
+
+// 	},
+// });
