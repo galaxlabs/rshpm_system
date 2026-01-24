a20221c4c572964258a971663373f559509b5999
galaxlabs <galaxylab2020@gmail.com>
2026-01-24 15:25:07 +0000

    feat:

---
+++ rshpm_system/rshpm_system/doctype/document_types/document_types.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Galaxy Labs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Document Types", {
+// 	refresh(frm) {
+
+// 	},
+// });
